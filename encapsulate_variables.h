1c2453d0a99a2264ace1ea16841459fab8098521
cyber-zeed <34373499+cyber-zeed@users.noreply.github.com>
2024-02-01 19:20:34 +0000

    Update encapsulate_variables.h

--- encapsulate_variables.h
+++ encapsulate_variables.h
@@ -8,19 +8,27 @@ String encapsulateVariables(int* digitalPins, int* analogPins) {
 
   // Encapsulate digital values
   for (int i = 0; i < 10; i++) {
+    int digitalValue = digitalRead(digitalPins[i]);
+    if (digitalValue != LOW && digitalValue != HIGH) {
+      // Handle unexpected digital values
+      digitalValue = -1;
+    }
+    
     message += "D";
     message += String(i);
     message += ":";
-    message += String(digitalRead(digitalPins[i]));
+    message += String(digitalValue);
     message += "&";
   }
 
   // Encapsulate analog values
   for (int i = 0; i < 5; i++) {
+    int analogValue = analogRead(analogPins[i]);
+    // Add range check for analog values if needed
     message += "A";
     message += String(i);
     message += ":";
-    message += String(analogRead(analogPins[i]));
+    message += String(analogValue);
     message += "&";
   }
 
