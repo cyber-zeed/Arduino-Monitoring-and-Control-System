91cc3ec5509d6f51a352f6bada731123863a31fe
cyber-zeed <34373499+cyber-zeed@users.noreply.github.com>
2024-02-01 19:21:53 +0000

    Update setkeys.h

--- setkeys.h
+++ setkeys.h
@@ -1,39 +1,34 @@
-#ifndef ENCAPSULATE_VARIABLES_H
-#define ENCAPSULATE_VARIABLES_H
+#ifndef SETDIGITALPINS_H
+#define SETDIGITALPINS_H
 
 #include <Arduino.h>
 
-String encapsulateVariables(int* digitalPins, int* analogPins) {
-  String message = "&begin&";
+void setDigitalPinState(int* digitalPins) {
+  while (Serial.available() > 0) {
+    String command = Serial.readStringUntil('\n');
+    if (command.startsWith("d") && command.length() > 3) {
+      int portNumber = command.substring(1, 2).toInt();
+      const int MIN_DIGITAL_PIN = 2;
+      const int MAX_DIGITAL_PIN = 11;
 
-  // Encapsulate digital values
-  for (int i = 0; i < 10; i++) {
-    int digitalValue = digitalRead(digitalPins[i]);
-    if (digitalValue != LOW && digitalValue != HIGH) {
-      // Handle unexpected digital values
-      digitalValue = -1;
+      if (portNumber >= MIN_DIGITAL_PIN && portNumber <= MAX_DIGITAL_PIN) {
+        String stateCommand = command.substring(2);
+        if (stateCommand == "on") {
+          digitalWrite(digitalPins[portNumber - MIN_DIGITAL_PIN], HIGH);
+          Serial.println("Port D" + String(portNumber) + " turned ON.");
+        } else if (stateCommand == "off") {
+          digitalWrite(digitalPins[portNumber - MIN_DIGITAL_PIN], LOW);
+          Serial.println("Port D" + String(portNumber) + " turned OFF.");
+        } else {
+          Serial.println("Invalid command for Port D" + String(portNumber) + ".");
+        }
+      } else {
+        Serial.println("Invalid port number.");
+      }
+    } else {
+      Serial.println("Invalid command format.");
     }
-    
-    message += "D";
-    message += String(i);
-    message += ":";
-    message += String(digitalValue);
-    message += "&";
   }
-
-  // Encapsulate analog values
-  for (int i = 0; i < 5; i++) {
-    int analogValue = analogRead(analogPins[i]);
-    // Add range check for analog values if needed
-    message += "A";
-    message += String(i);
-    message += ":";
-    message += String(analogValue);
-    message += "&";
-  }
-
-  message += "&end&";
-  return message;
 }
 
-#endif // ENCAPSULATE_VARIABLES_H
+#endif // SETDIGITALPINS_H
